3de346d022ab548d49d85e5cacc4600f9b44e633
Peng He <pha58@sfu.ca>
2026-07-24 11:16:22 -0700

    Expose presence_penalty and topk to upstream code

--- lcb_runner/runner/parser.py
+++ lcb_runner/runner/parser.py
@@ -59,6 +59,8 @@ def get_args():
         "--temperature", type=float, default=0.2, help="Temperature for sampling"
     )
     parser.add_argument("--top_p", type=float, default=0.95, help="Top p for sampling")
+    parser.add_argument("--top_k", type=int, default=-1, help="Top k for sampling")
+    parser.add_argument("--presence_penalty", type=float, default=0.0, help="Presence_penalty for sampling")
     parser.add_argument(
         "--max_tokens", type=int, default=2000, help="Max tokens for sampling"
     )
